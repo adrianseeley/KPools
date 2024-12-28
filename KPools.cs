a6252dbc03f9fcb6f2b29f50c4593810e09da2c4
Adrian Seeley <contact@adrianseeley.com>
2024-12-28 17:47:54 -0500

    testing kpools

---
+++ KPools.cs
@@ -0,0 +1,107 @@
+﻿public class KPools
+{
+    public Random random;
+    public int k;
+    public int dimensionCount;
+    public int poolCount;
+    public List<(List<float> input, List<float> output)> samples;
+    public List<KPool> kPools;
+
+    public KPools(int k, int dimensionCount, int indiciesPerPool, int poolCount, List<(List<float> input, List<float> output)> samples)
+    {
+        this.random = new Random();
+        this.k = k;
+        this.dimensionCount = dimensionCount;
+        this.samples = samples;
+        this.kPools = new List<KPool>();
+        int[] allDimensions = Enumerable.Range(0, samples[0].input.Count).ToArray();
+        for (int p = 0; p < poolCount; p++)
+        {
+            int[] dimensions = allDimensions.OrderBy(x => random.Next()).Take(dimensionCount).ToArray();
+            int[] indices = Enumerable.Range(0, indiciesPerPool).OrderBy(x => random.Next()).Take(indiciesPerPool).ToArray();
+            kPools.Add(new KPool(k, dimensions, indices));
+        }
+    }
+
+    public List<float> Predict(List<float> input)
+    {
+        List<float> output = new List<float>(samples[0].output.Count);
+        for (int i = 0; i < samples[0].output.Count; i++)
+        {
+            output.Add(0);
+        }
+        foreach (KPool kPool in kPools)
+        {
+            List<float> kOutput = kPool.Predict(samples, input);
+            for (int i = 0; i < samples[0].output.Count; i++)
+            {
+                output[i] += kOutput[i];
+            }
+        }
+        for (int i = 0; i < samples[0].output.Count; i++)
+        {
+            output[i] /= kPools.Count;
+        }
+        return output;
+    }
+}
+
+public class  KPool
+{
+    public int k;
+    public int[] dimensions;
+    public int[] indices;
+
+    public KPool(int k, int[] dimensions, int[] indices)
+    {
+        this.k = k;
+        this.dimensions = dimensions;
+        this.indices = indices;
+    }
+
+    public List<float> Predict(List<(List<float> input, List<float> output)> samples, List<float> input)
+    {
+        List<IndexDistance> indexDistances = new List<IndexDistance>();
+        foreach(int index in indices)
+        {
+            float distance = 0;
+            foreach (int dimension in dimensions)
+            {
+                distance += MathF.Pow(input[dimension] - samples[index].input[dimension], 2);
+            }
+            distance = MathF.Sqrt(distance);
+            indexDistances.Add(new IndexDistance(index, distance));
+        }
+        indexDistances.Sort((a, b) => a.distance.CompareTo(b.distance));
+        List<float> output = new List<float>(samples[0].output.Count);
+        for (int i = 0; i < samples[0].output.Count; i++)
+        {
+            output.Add(0);
+        }
+        for (int i = 0; i < k; i++)
+        {
+            for (int j = 0; j < samples[0].output.Count; j++)
+            {
+                output[j] += samples[indexDistances[i].index].output[j];
+            }
+        }
+        for (int i = 0; i < samples[0].output.Count; i++)
+        {
+            output[i] /= k;
+        }
+        return output;
+    }
+}
+
+public class IndexDistance
+{
+    public int index;
+    public float distance;
+
+    public IndexDistance(int index, float distance)
+    {
+        this.index = index;
+        this.distance = distance;
+    }
+}
+
---
+++ Program.cs
@@ -0,0 +1,102 @@
+﻿public class Program
+{
+    public static List<(List<float> input, List<float> output)> ReadMNIST(string filename, int max = -1)
+    {
+        List<(List<float> input, List<float> output)> data = new List<(List<float> input, List<float> output)>();
+        string[] lines = File.ReadAllLines(filename);
+        for (int lineIndex = 1; lineIndex < lines.Length; lineIndex++) // skip headers
+        {
+            string line = lines[lineIndex].Trim();
+            if (line.Length == 0)
+            {
+                continue; // skip empty lines
+            }
+            string[] parts = line.Split(',');
+            int labelInt = int.Parse(parts[0]);
+            List<float> labelOneHot = new List<float>();
+            for (int i = 0; i <= 9; i++)
+            {
+                if (i == labelInt)
+                {
+                    labelOneHot.Add(1);
+                }
+                else
+                {
+                    labelOneHot.Add(0);
+                }
+            }
+            List<float> input = new List<float>();
+            for (int i = 1; i < parts.Length; i++)
+            {
+                input.Add(float.Parse(parts[i]));
+            }
+            data.Add((input, labelOneHot));
+            if (max != -1 && data.Count >= max)
+            {
+                break;
+            }
+        }
+        return data;
+    }
+
+    public static float OneHotFitness(KPools kPools, List<(List<float> input, List<float> output)> test, bool verbose)
+    {
+        object trackLock = new object();
+        int correct = 0;
+        int incorrect = 0;
+        Parallel.For(0, test.Count, (index) =>
+        {
+            int localIndex = index;
+            (List<float> input, List<float> output) sample = test[localIndex];
+            List<float> predictOutput = kPools.Predict(sample.input);
+            int predictLabel = predictOutput.IndexOf(predictOutput.Max());
+            int actualLabel = sample.output.IndexOf(sample.output.Max());
+            lock (trackLock)
+            {
+                if (predictLabel == actualLabel)
+                {
+                    correct++;
+                }
+                else
+                {
+                    incorrect++;
+                }
+                if (verbose)
+                {
+                    Console.Write($"\rCorrect: {correct}, Incorrect: {incorrect}, Fitness: {((float)correct / (correct + incorrect))}");
+                }
+            }
+        });
+        if (verbose)
+        {
+            Console.WriteLine();
+        }
+        return ((float)correct) / ((float)test.Count);
+    }
+
+    public static void Main()
+    {
+        List<(List<float> input, List<float> output)> mnistTrain = ReadMNIST("D:/data/mnist_train.csv", max: 1000);
+        List<(List<float> input, List<float> output)> mnistTest = ReadMNIST("D:/data/mnist_test.csv");
+
+        using TextWriter tw = new StreamWriter("results.csv");
+        tw.WriteLine("k,dimensionCount,indicesPerPool,poolCount,fitness");
+        for (int k = 1; k <= 10; k++)
+        {
+            for (int dimensionCount = 1; dimensionCount <= 30; dimensionCount++)
+            {
+                for (int indicesPerPool = k * 2; indicesPerPool <= 500; indicesPerPool += 5)
+                {
+                    for (int poolCount = 1; poolCount <= 1000; poolCount += 5)
+                    {
+                        KPools kPools = new KPools(k: k, dimensionCount: dimensionCount, indiciesPerPool: indicesPerPool, poolCount: poolCount, samples: mnistTrain);
+                        float fitness = OneHotFitness(kPools, mnistTest, verbose: false);
+                        tw.WriteLine($"{k},{dimensionCount},{indicesPerPool},{poolCount},{fitness}");
+                        tw.Flush();
+                    }
+                }
+            }
+        }
+    }
+}
+
